44f6d126cf16da628ed45b5f6d05338911a2192c
Mike Hagendoorn <kaanmijo@gmail.com>
2016-09-27 16:20:49 +0200

    Added Spring, Maven etc..

---
+++ src/main/java/nl/avans/ivh5/FysioApplication.java
@@ -0,0 +1,12 @@
+package nl.avans.ivh5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FysioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FysioApplication.class, args);
+	}
+}
